89b57f615263594320dd5f39e0d673c6e44aa19c
HARFHO <Harfho77@gmail.com>
2021-07-03 15:35:55 +0200

    Update AutoFCB(Mbutt).user.js

--- AutoFCB(Mbutt).user.js
+++ AutoFCB(Mbutt).user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AutoFCB(Mbutt)
 // @namespace    https://github.com/Acotec/autofcb
-// @version      0.7.11
+// @version      0.7.14
 // @description  Open Remain buttons after Auto(FCB) is run , (optional)
 // @author       Acotec
 // @updateURL    https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(Mbutt).user.js
@@ -11,7 +11,5 @@
 // @grant        GM_getValue
 // @grant        GM_setClipboard
 // @grant        window.close
-// @require      http://code.jquery.com/jquery-3.5.1.min.js
 // @require      https://github.com/Acotec/autofcb_script/raw/master/AutoFCB(Mbutt).user.js
 // ==/UserScript==
-(function () {})();
