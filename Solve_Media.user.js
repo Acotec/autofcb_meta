7bd6468eb6f4f1078eaaf4b4ab06ff4229d770e7
HARFHO <Harfho77@gmail.com>
2021-07-17 07:14:18 +1000

    Create Solve_Media.user.js

---
+++ Solve_Media.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Solved_Media
+// @namespace   https://github.com/Acotec/autofcb
+// @description Autocompletion for Solve Media image CAPTCHAs.
+// @author      Acotec
+// @license     MIT
+// @include     *auto*.*
+// @run-at      document-end
+// @require     https://github.com/Acotec/require/raw/master/waitForKeyElements.min.js
+// @resource    _phrases https://github.com/Acotec/resources/raw/master/_phrases.txt
+// @homepageURL https://eugenox.appspot.com/script/solvedmedia
+// @updateURL   https://github.com/Acotec/autofcb_meta/raw/master/Solve_Media.user.js
+// @downloadURL https://github.com/Acotec/autofcb_meta/raw/master/Solve_Media.user.js
+// @grant       GM_getResourceText
+// ==/UserScript==
+
+(function(){ })();
