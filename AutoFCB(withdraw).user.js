cc34c7c9acab4b296654aa3d959b8ef867409a26
HARFHO <Harfho77@gmail.com>
2021-07-17 08:47:20 +1100

    Update AutoFCB(withdraw).user.js

--- AutoFCB(withdraw).user.js
+++ AutoFCB(withdraw).user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name         AutoFCB(withdraw)
-// @description  withdraw
-// @author       Acotec
-// @include      *auto*/dashboard/claim/manual
-// @include      *auto*/dashboard/withdraw/*
-// @updateURL    https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(withdraw).user.js
-// @downloadURL  https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(withdraw).user.js
-// @require      https://github.com/Acotec/require/raw/master/waitForKeyElements.min.js
-// @grant        none
-
-// @require     https://github.com/Acotec/autofcb_script/raw/master/AutoFCB(withdraw).user.js
+// @name          AutoFCB(withdraw)
+// @description   withdraw
+// @author        Acotec
+// @include       *auto*/dashboard/claim/manual*
+// @include       *auto*/dashboard/withdraw/*
+// @updateURL     https://github.com/Acotec/autofcb/raw/master/AutoFCB(withdraw).user.js
+// @downloadURL   https://github.com/Acotec/autofcb/raw/master/AutoFCB(withdraw).user.js
+// @require       https://github.com/Acotec/require/raw/master/waitForKeyElements.min.js
+// @grant         GM_setValue
+// @grant         GM_getValue
+// @require      https://github.com/Acotec/autofcb_script/raw/master/AutoFCB(withdraw).user.js
 // ==/UserScript==
-(function(){ })();
+(function(){})();
