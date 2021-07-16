bd0755fa2e45df7b75f12407834929ff3ad54bb1
HARFHO <Harfho77@gmail.com>
2021-07-17 02:18:08 +0500

    Update Solve_Media.user.js

--- Solve_Media.user.js
+++ Solve_Media.user.js
@@ -7,8 +7,8 @@
 // @include     *auto*.*
 // @run-at      document-end
 // @require     https://github.com/Acotec/require/raw/master/waitForKeyElements.min.js
+// @require     https://github.com/Acotec/autofcb_script/raw/master/Solve_Media.user.js
 // @resource    _phrases https://github.com/Acotec/resources/raw/master/_phrases.txt
-// @homepageURL https://eugenox.appspot.com/script/solvedmedia
 // @updateURL   https://github.com/Acotec/autofcb_meta/raw/master/Solve_Media.user.js
 // @downloadURL https://github.com/Acotec/autofcb_meta/raw/master/Solve_Media.user.js
 // @grant       GM_getResourceText
