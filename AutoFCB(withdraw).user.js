8b14ac9389a90672b5df4a9ce8dee0608ccc95d8
HARFHO <Harfho77@gmail.com>
2021-07-17 05:49:04 +0800

    Update AutoFCB(withdraw).user.js

--- AutoFCB(withdraw).user.js
+++ AutoFCB(withdraw).user.js
@@ -4,8 +4,8 @@
 // @author        Acotec
 // @include       *auto*/dashboard/claim/manual*
 // @include       *auto*/dashboard/withdraw/*
-// @updateURL     https://github.com/Acotec/autofcb/raw/master/AutoFCB(withdraw).user.js
-// @downloadURL   https://github.com/Acotec/autofcb/raw/master/AutoFCB(withdraw).user.js
+// @updateURL     https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(withdraw).user.js
+// @downloadURL   https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(withdraw).user.js
 // @require       https://github.com/Acotec/require/raw/master/waitForKeyElements.min.js
 // @grant         GM_setValue
 // @grant         GM_getValue
