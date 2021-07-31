226029c933b877059f3c097b5d937b7995357f4b
HARFHO <Harfho77@gmail.com>
2021-08-01 03:25:56 +1200

    Update AutoFCB(Close).user.js

--- AutoFCB(Close).user.js
+++ AutoFCB(Close).user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name         AutoFCB(Close)
 // @namespace    https://github.com/Acotec/autofcb
-// @version      1.0.7
+// @version      1.0.8
 // @description  Auto closeDone Auto**/
 // @author       Acotec
 // @updateURL    https://github.com/Acotec/autofcb/raw/master/AutoFCB(Close).user.js
 // @downloadURL  https://github.com/Acotec/autofcb/raw/master/AutoFCB(Close).user.js
-// @require      https://github.com/Acotec/require/raw/master/cookie_sg_min.js
 // @include      *auto*/dashboard/shortlinks
 // @grant        window.close
 // @run-at       document-start
