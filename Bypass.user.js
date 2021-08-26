9ba853d662378f4a1c5b2158b1f3feb8021cd9dd
HARFHO <Harfho77@gmail.com>
2021-08-26 13:50:51 +0100

    Update Bypass.user.js

--- Bypass.user.js
+++ Bypass.user.js
@@ -1,19 +1,21 @@
 // ==UserScript==
 // @name         Bypass
 // @namespace    https://github.com/Acotec/autofcb
-// @version      0.1.1
+// @version      0.1.2
 // @description  Bypass URL links
 // @author       Acotec
 // @updateURL    https://github.com/Acotec/autofcb_meta/raw/master/Bypass.user.js
 // @downloadURL  https://github.com/Acotec/autofcb_meta/raw/master/Bypass.user.js
 // @match        *://*/*
 // @resource     key https://raw.githubusercontent.com/Acotec/resources/master/keyEncode.txt
-// @require      https://github.com/Acotec/autofcb_script/raw/master/Bypass.user.js
+// @grant        GM_addElement
 // @grant        GM_setValue
+// @grant        GM_setClipboard
+// @grant        GM_notification
 // @grant        GM_getValue
-// @grant        GM_xmlhttpRequest
-// @grant        GM_getResourceText
-// @run_at       document-start
+// @grant        window.close
+// @run-at       document-start
 // @connect      https://api.yuumari.com/
+// @require      https://github.com/Acotec/autofcb_script/raw/master/Bypass.user.js
 // ==/UserScript==
 (function(){})();
