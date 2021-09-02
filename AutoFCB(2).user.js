7a105cfe7518a0628c39f31778d58ef2af565af4
HARFHO <Harfho77@gmail.com>
2021-09-02 18:25:22 +0100

    Update AutoFCB(2).user.js
    add Function to auto check the short links that won't be open

--- AutoFCB(2).user.js
+++ AutoFCB(2).user.js
@@ -1,19 +1,22 @@
 // ==UserScript==
 // @name         AutoFCB(2)
 // @namespace    https://github.com/Acotec/autofcb
-// @version      2.1.9
+// @version      2.1.10
 // @description  AutomateButtons
 // @author       Acotec
 // @updateURL    https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(2).user.js
 // @downloadURL  https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(2).user.js
 // @require      http://code.jquery.com/jquery-3.5.1.min.js
-// @resource     _DontOpen https://gist.githubusercontent.com/Harfho/d4805d8a56793fa59d47e464c6eec243/raw/_DontOpen.txt
+// @connect      gist.githubusercontent.com
+//// @resource     _DontOpen https://gist.githubusercontent.com/Harfho/d4805d8a56793fa59d47e464c6eec243/raw/_DontOpen.txt
 // @include      *auto*/shortlinks
+// @noframes
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_getResourceText
 // @grant        unsafeWindow
 // @grant        window.close
+// @grant        GM_xmlhttpRequest
 // @require      https://github.com/Acotec/autofcb_script/raw/master/AutoFCB(2).user.js
 // ==/UserScript==
 (function() {})();
