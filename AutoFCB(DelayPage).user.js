59f28364449237ab417953df7fff37cc73912eed
HARFHO <Harfho77@gmail.com>
2021-09-02 02:07:01 +0100

    Update AutoFCB(DelayPage).user.js

--- AutoFCB(DelayPage).user.js
+++ AutoFCB(DelayPage).user.js
@@ -1,20 +1,15 @@
 // ==UserScript==
 // @name         AutoFCB(DelayPage)
 // @namespace    https://github.com/Acotec/autofcb
-// @version      0.7.20
-// @description  Delay some shortlink page loading time and close it when done
+// @version      0.7.21
+// @description  Delay some shortlink page loading time
 // @author       Acotec
 // @updateURL    https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(DelayPage).user.js
 // @downloadURL  https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(DelayPage).user.js
-// @require      https://github.com/Acotec/require/raw/master/Super_GM.user.js
-// @require      https://github.com/Acotec/require/raw/master/waitForKeyElements.min.js
 // @resource     delaypage https://github.com/Acotec/resources/raw/master/delaypage.txt
 // @match        *://*/*
-// @grant        GM_setValue
-// @grant        GM_getValue
 // @grant        GM_getResourceText
 // @grant        window.close
-// @grant        unsafeWindow
 // @run-at       document-start
 // @require      https://github.com/Acotec/autofcb_script/raw/master/AutoFCB(DelayPage).user.js
 // ==/UserScript==
