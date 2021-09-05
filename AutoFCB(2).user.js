ae681e62565d306e9a2bf1a041b0dcb22ffbf24e
HARFHO <Harfho77@gmail.com>
2021-09-05 01:24:47 +0100

    Update AutoFCB(2).user.js

--- AutoFCB(2).user.js
+++ AutoFCB(2).user.js
@@ -6,7 +6,7 @@
 // @author       Acotec
 // @updateURL    https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(2).user.js
 // @downloadURL  https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(2).user.js
-// @require      http://code.jquery.com/jquery-3.5.1.min.js
+//// @require      http://code.jquery.com/jquery-3.5.1.min.js
 // @connect      *
 //// @resource     _DontOpen https://gist.githubusercontent.com/Harfho/d4805d8a56793fa59d47e464c6eec243/raw/_DontOpen.txt
 // @include      *auto*/shortlinks
