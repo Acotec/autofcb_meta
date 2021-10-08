74ed50e021482f9961989544b4a6185af6506f43
HARFHO <Harfho77@gmail.com>
2021-10-08 22:02:16 +0100

    Create AutoFCB(signin).user.js

---
+++ AutoFCB(signin).user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         AutoFCB(signin)
+// @namespace    Acotec
+// @version      0.1
+// @description  select solvemdia Automatically on signin/signup page
+// @author       Acotec
+// @updateURL    https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(signin).user.js
+// @downloadURL  https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(signin).user.js
+// @include      *auto*/signin
+// @icon         https://www.google.com/s2/favicons?domain=autofaucet.org
+// @require      https://github.com/Acotec/autofcb_script/raw/master/AutoFCB(signin).user.js
+// ==/UserScript==
+(function(){})();
