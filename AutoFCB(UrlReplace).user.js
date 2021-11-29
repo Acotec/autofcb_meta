5f59d08ef402f69df37339c6a0f41562ef616eb8
HARFHO <Harfho77@gmail.com>
2021-11-29 15:33:12 +0100

    Update AutoFCB(UrlReplace).user.js

--- AutoFCB(UrlReplace).user.js
+++ AutoFCB(UrlReplace).user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         AutoFCB(UrlReplace)
 // @namespace    https://github.com/Acotec/autofcb
-// @version      1.2.6
+// @version      1.2.7
 // @description  Auto ReplaceUrl of Shortlink sites
 // @author       Acotec
 // @updateURL    https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(UrlReplace).user.js
 // @downloadURL  https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(UrlReplace).user.js
-// @require      https://github.com/Acotec/require/raw/master/waitForKeyElements.min.js
-// @require      https://cdn.jsdelivr.net/npm/js-base64@3.6.1/base64.min.js
+//// @require      https://github.com/Acotec/require/raw/master/waitForKeyElements.min.js
+//// @require      https://cdn.jsdelivr.net/npm/js-base64@3.6.1/base64.min.js
 // @match        *://*/*
 // @grant        unsafeWindow
 // @run-at       document-start
