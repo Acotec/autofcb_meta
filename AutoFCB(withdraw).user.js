7c7a26f5bb699761f48e54fd03430b0d5bf309c5
HARFHO <Harfho77@gmail.com>
2021-12-22 15:45:04 +0100

    Update AutoFCB(withdraw).user.js

--- AutoFCB(withdraw).user.js
+++ AutoFCB(withdraw).user.js
@@ -1,9 +1,12 @@
 // ==UserScript==
 // @name          AutoFCB(withdraw)
 // @description   withdraw
+// @version       0.2
 // @author        Acotec
 // @include       *auto*/dashboard/claim/manual*
 // @include       *auto*/dashboard/withdraw/*
+// @include       *auto*/dashboard/settings
+// @include       *auto*/dashboard/withdraw
 // @updateURL     https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(withdraw).user.js
 // @downloadURL   https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(withdraw).user.js
 // @require       https://github.com/Acotec/require/raw/master/waitForKeyElements.min.js
