807c5bfd476f778d34a9013377df8850c5c15019
HARFHO <Harfho77@gmail.com>
2022-01-12 05:04:59 +0100

    Update AutoFCB(DelayPage).user.js

--- AutoFCB(DelayPage).user.js
+++ AutoFCB(DelayPage).user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         AutoFCB(DelayPage)
 // @namespace    https://github.com/Acotec/autofcb
-// @version      0.7.22
+// @version      0.7.23
 // @description  Delay some shortlink page loading time
 // @author       Acotec
 // @updateURL    https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(DelayPage).user.js
 // @downloadURL  https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(DelayPage).user.js
-// @resource     delaypage https://gist.githubusercontent.com/Harfho/e6ed9bbe9feb74e71030c680feba9d71/raw/delaypage.txt
+// @resource     delaypage https://gist.githubusercontent.com/Harfho/8f5a3bd519f0ebf94708ad624ffd76d2/raw/delaypage.txt
 // @match        *://*/*
 // @grant        GM_getResourceText
 // @grant        window.close
