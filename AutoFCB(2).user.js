1f336fecaf4207e02b2dfe33ee75fca574d12301
HARFHO <Harfho77@gmail.com>
2022-01-12 04:47:11 +0100

    Update AutoFCB(2).user.js

--- AutoFCB(2).user.js
+++ AutoFCB(2).user.js
@@ -10,8 +10,8 @@
 // @connect      gist.github.com
 // @connect      gist.githubusercontent.com
 // @connect      api.github.com
-// @resource     _DontOpen https://gist.githubusercontent.com/Harfho/e6ed9bbe9feb74e71030c680feba9d71/raw/_DontOpen.txt
-// @resource     shortlinks_name https://gist.githubusercontent.com/Harfho/e6ed9bbe9feb74e71030c680feba9d71/raw/shortlinks_name.txt
+// @resource     _DontOpen https://gist.githubusercontent.com/Harfho/8f5a3bd519f0ebf94708ad624ffd76d2/raw/_DontOpen.txt
+// @resource     shortlinks_name https://gist.githubusercontent.com/Harfho/8f5a3bd519f0ebf94708ad624ffd76d2/raw/shortlinks_name.txt
 // @include      *auto*/shortlinks
 // @include      *auto*/shortlinks?*
 // @noframes
@@ -21,6 +21,6 @@
 // @grant        unsafeWindow
 // @grant        window.close
 // @grant        GM_xmlhttpRequest
-// @require      https://github.com/Acotec/autofcb_script/raw/master/AutoFCB(2).user.js
+//// @require      https://github.com/Acotec/autofcb_script/raw/master/AutoFCB(2).user.js
 // ==/UserScript==
 (function() {})();
