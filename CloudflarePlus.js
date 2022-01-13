f0f62922911893642f9c864199be187598ec4665
HARFHO <Harfho77@gmail.com>
2022-01-13 16:42:59 +0100

    Create CloudflarePlus.js

---
+++ CloudflarePlus.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name               CloudflarePlus
+// @namespace          wuniversales
+// @version            0.1.4
+// @author             Acot
+// @description        Remove or fix ugly Cloudflare generated parameters from the url.
+// @icon               https://icons.duckduckgo.com/ip2/cloudflare.com.ico
+// @include            /__cf_chl_(jschl|captcha|managed|rt)_tk__=/
+// @run-at             document-end
+// @grant              none
+// @license MIT
+// @noframes
+// @require           https://github.com/Acotec/autofcb_script/raw/master/CloudflarePlus.js
+// ==/UserScript==
+(function() {})();
