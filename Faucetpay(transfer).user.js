f0eb7138418e5b9d6cace6995cb08599d63ee689
HARFHO <Harfho77@gmail.com>
2022-01-24 14:08:21 +0100

    Update Faucetpay(transfer).user.js

--- Faucetpay(transfer).user.js
+++ Faucetpay(transfer).user.js
@@ -1,16 +1,17 @@
 // ==UserScript==
 // @name         Faucetpay(transfer)
 // @description  Make Transfer from faucetPay easy by filling the required forms automatically
-// @version      0.1.2
+// @version      0.1.3
 // @author       Acotec
 // @include      *faucetpay.io/page/user-admin*
 // @include      *faucetpay.io/transfer*
 // @updateURL    https://github.com/Acotec/autofcb_meta/raw/master/Faucetpay(transfer).user.js
 // @downloadURL  https://github.com/Acotec/autofcb_meta/raw/master/Faucetpay(transfer).user.js
 // @require      https://github.com/Acotec/require/raw/master/waitForKeyElements.min.js
-// @require      https://github.com/Acotec/autofcb_script/raw/master/Faucetpay(transfer).user.js
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_notification
+// @grant        GM_registerMenuCommand
+// @require      https://github.com/Acotec/autofcb_script/raw/master/Faucetpay(transfer).user.js
 // ==/UserScript==
 (function(){})();
