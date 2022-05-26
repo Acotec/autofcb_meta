fdb36de77f0a440673aea75949b4dbcb83538465
HARFHO <Harfho77@gmail.com>
2022-05-26 08:11:52 +0100

    Update AutoFCB(ReloadShortlinkPage).user.js

--- AutoFCB(ReloadShortlinkPage).user.js
+++ AutoFCB(ReloadShortlinkPage).user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AutoFCB(ReloadShortlinkPage)
 // @namespace    Acotec
-// @version      0.1.1
+// @version      0.1.2
 // @description  Refresh page after every 3minute for 3time and stop
 // @author       Acotec
 // @updateURL    https://github.com/Acotec/autofcb_meta/raw/master/AutoFCB(ReloadShortlinkPage).user.js
@@ -14,6 +14,7 @@
 // @grant        GM_getValue
 // @grant        window.close
 // @run-at       document-start
+// @noframes
 // @require      https://github.com/Acotec/autofcb_script/raw/master/AutoFCB(ReloadShortlinkPage).user.js
 // ==/UserScript==
 (function(){})();
